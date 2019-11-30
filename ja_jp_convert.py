c9e78dd22627861132d5684a8e7a9aee0e1d59d5
Alex Poon <mundanesurrealism@gmail.com>
2019-11-30 20:28:06 +0800

    Update ja_jp_convert.py

--- ja_jp_convert.py
+++ ja_jp_convert.py
@@ -19,4 +19,4 @@ for key in j:
 	j[key] = conv.do(j[key]).replace('aa','ā').replace('ou','ō').replace('ii','ī').replace('uu','ū')
 
 with open('ja_rj.json', 'w') as f:
-	f.write(d(j))
+	f.write(d(j, indent=4))
